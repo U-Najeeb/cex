5cbd0f42252d5bb1ad712c099f9821710a71478a
U-Najeeb <umair.najeeb@iquasar.com>
2024-08-06 19:50:10 +0530

    Feat : Completed Login Page Prisma Setup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import db from "@/app/db/PrismaClient";
+import { Keypair } from "@solana/web3.js";
 
 const handler = NextAuth({
   providers: [
@@ -8,6 +10,55 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }: any) {
+      if (account?.provider === "google") {
+        const email = user.email;
+        if (!email) {
+          return false;
+        }
+
+        const userDb = await db.user.findFirst({
+          where: {
+            username: email,
+          },
+        });
+
+        if (userDb) {
+          return true;
+        }
+
+        const keypair = Keypair.generate();
+        const publicKey = keypair.publicKey.toBase58();
+        const privateKey = keypair.secretKey;
+
+        await db.user.create({
+          data: {
+            username: email,
+            //@ts-ignore
+            profilePicture: profile?.picture,
+            provider: "Google",
+
+            SolWallet: {
+              create: {
+                publicKey: publicKey,
+                privateKey: privateKey.toString(),
+              },
+            },
+            InrWallet: {
+              create: {
+                balance: 0,
+              },
+            },
+          },
+        });
+
+        return true;
+      }
+
+      return false;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,30 +2,44 @@
 
 import React from "react";
 import { Button } from "./ui/Button";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function Hero() {
+  const session = useSession();
+  const router = useRouter();
   return (
     <div className="flex flex-col justify-center">
       <div className="text-6xl font-medium">
         <span>The Indian Cryptocurrency</span>
         <span className="text-blue-500 pl-4">Revolution</span>
       </div>
-      <div className="flex justify-center text-2xl text-slate-500 pt-6">
+      <div className="flex justify-center text-xl text-slate-500 pt-6">
         Create a frictionless wallet with just a Google Account.
       </div>
-      <div className="flex justify-center text-xl text-slate-500 pt-2">
+      <div className="flex justify-center text-base text-slate-500 pt-2">
         Convert your INR into Cyptocurrency
       </div>
       <div className="flex justify-center pt-4">
-        <Button
-          className="bg-blue-600 "
-          onClick={() => {
-            signIn("google");
-          }}
-        >
-          Login with Google
-        </Button>
+        {!session.data?.user ? (
+          <Button
+            className="bg-blue-600 "
+            onClick={() => {
+              signIn("google");
+            }}
+          >
+            Login with Google
+          </Button>
+        ) : (
+          <Button
+            className="bg-blue-600 "
+            onClick={() => {
+              router.push("/dashboard");
+            }}
+          >
+            Go to Dashboard
+          </Button>
+        )}
       </div>
     </div>
   );
